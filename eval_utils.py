d42565f691c06d3596aa58183a666ac2dc913040
zhangir-azerbayev <59542043+zhangir-azerbayev@users.noreply.github.com>
2022-05-01 23:25:05 -0400

    Update eval_utils.py

--- eval_utils.py
+++ eval_utils.py
@@ -1,6 +1,10 @@
 import re 
 import numpy as np
 
+def batch_loader(seq, size):
+    return [seq[pos:pos + size] for pos in range(0, len(seq), size)]
+
+
 def pass_k(lst, k): 
     """
     lst: Boolean list 
