aae7942a67e37d031b79a5993c285ea8bc121a20
NickSchoelkopf <nick.schoelkopf@yale.edu>
2022-05-31 19:39:40 -0400

    add validation to fully supervised training

--- etk/full_supervision/train_student.py
+++ etk/full_supervision/train_student.py
@@ -3,15 +3,17 @@ import os
 import yaml 
 import math
 import json
+from tqdm import tqdm
 
-from etk.data.mathqa_dataset import MathQATrainSet
+from etk.data.mathqa_dataset import MathQATrainSet, read_gsm8k
+from etk.eval_utils import tokens_to_gsm8k_log_entry, batch_loader
 
 import torch 
 import torch.nn
 from torch.optim.lr_scheduler import LambdaLR
 
 from transformers import GPTNeoForCausalLM, GPT2Tokenizer, AutoModelForCausalLM, AutoTokenizer
-from transformers import TrainingArguments, Trainer 
+from transformers import Seq2SeqTrainingArguments, Seq2SeqTrainer 
 from transformers import AdamW
 from transformers.trainer_pt_utils import get_parameter_names
 
@@ -48,12 +50,14 @@ config_path = sys.argv[1]
 with open(config_path, "r") as f: 
     cfg = yaml.safe_load(f)
 
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
 os.environ["CUDA_VISIBLE_DEVICES"] = cfg["devices"]
 experiment_name = cfg["experiment_name"]
 teacher_data_path = cfg["teacher_data_path"]
 lr = cfg["lr"]
 epochs = cfg["epochs"]
 batch_size = cfg["batch_size"]
+eval_batch_size = cfg["eval_batch_size"]
 weight_decay = cfg["weight_decay"]
 model_name = cfg["model_name"]
 model_type = cfg["model_type"] 
@@ -64,6 +68,10 @@ elif model_type=="incoder":
 else: 
     raise ValueError("invalid `model_type`")
 max_length = cfg["max_length"]
+# config values to be used for validation
+max_generation_length = cfg["max_gen_tokens"] + cfg["max_length"]
+num_samples = cfg["num_samples"]
+temp = cfg["temp"]
 
 results_dir = f"train_results/{experiment_name}"
 os.mkdir(results_dir)
@@ -73,6 +81,7 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 tokenizer.pad_token = tokenizer.eos_token
 
 dataset = load_trainset_from_log(teacher_data_path, tokenizer, max_length)
+eval_dataset = read_gsm8k("../data/gsm8k/gsm8k_dev.jsonl")
 
 #Loads model
 model = AutoModelForCausalLM.from_pretrained(model_name)
@@ -102,16 +111,79 @@ with open(os.path.join(results_dir, "config.yml"), "w") as f:
     yaml.dump(cfg, f)
 
 steps_per_epoch = math.ceil(len(dataset)/batch_size)
-training_args = TrainingArguments(output_dir=results_dir,
+training_args = Seq2SeqTrainingArguments(output_dir=results_dir,
                                   num_train_epochs=epochs,
+                                  evaluation_strategy="epoch",
                                   per_device_train_batch_size=batch_size,
+                                  per_device_eval_batch_size=eval_batch_size,
                                   logging_steps=steps_per_epoch*5,
                                   save_steps=steps_per_epoch*5,
-                                  warmup_steps = 100,
+                                  warmup_steps=100,
+                                  remove_unused_columns=False,
                                   )
 
+class TrainerWithEval(Seq2SeqTrainer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def evaluate(self, eval_dataset=None, **kwargs):
+        "Overrides the regular Seq2SeqTrainer.evaluate() since do_sample cannot be set via a training argument."
+        print("Validating...")
+        log = []
+        eval_dataset = self.eval_dataset
+        dataloader = batch_loader(eval_dataset, self.args.per_device_eval_batch_size) 
+        for batch in tqdm(dataloader): 
+            labels = [instance.answer for instance in batch]
+            texts = [instance.text for instance in batch]
+            task_ids = [instance.task_id for instance in batch]
+
+            batch_length = len(texts)
+
+            encoded_texts = tokenizer(texts, 
+                                return_tensors="pt", 
+                                max_length=max_length, 
+                                truncation=True,
+                                padding='max_length', 
+                                ).to("cuda")
+
+            prompt_lens = [torch.sum(x) for x in encoded_texts["attention_mask"]]
+
+            outputs = model.generate(**encoded_texts, 
+                                    do_sample=True, 
+                                    temperature=temp, 
+                                    max_new_tokens=cfg["max_gen_tokens"],
+                                    num_return_sequences=num_samples,
+                                    pad_token_id=tokenizer.eos_token_id
+                                    )
+            
+
+            outputs = torch.reshape(outputs, (batch_length, num_samples, -1))
+            
+
+            for text, task_id, label, outs, prompt_len in zip(texts, 
+                    task_ids, labels, outputs, prompt_lens): 
+
+                log_entry, _ = tokens_to_gsm8k_log_entry(outs, 
+                                                        label,
+                                                        task_id,
+                                                        text,
+                                                        prompt_len, 
+                                                        tokenizer, 
+                                                        model_type,
+                                                        verbose=False)
+
+                log.append(log_entry)
+                
+        num_examples = len(log)
+        num_passed = sum([x["passk"] for x in log])
+        pass_k = num_passed/num_examples
+        pass_1 = sum([x["pass1"] for x in log])/num_examples
+        print(f"pass@{num_samples}: {pass_k}\n")
+        return {f"pass@{num_samples}": pass_k}
+    
 
 # Runs training 
-Trainer(model=model, args=training_args, train_dataset=dataset,
+TrainerWithEval(model=model, args=training_args, train_dataset=dataset,
+        eval_dataset=eval_dataset,
         data_collator=data_collator,
         optimizers=(optimizer, scheduler)).train()
